3ee888a066107fd37d8bd309bac2584b7baea311
Dhaneesh K Kuniyil <dhaneesh.krishnan@gmail.com>
2023-06-14 23:53:45 -0600

    re-arranged app.py

--- app.py
+++ app.py
@@ -11,17 +11,6 @@ app = Flask(__name__)
 prompt_example = "I am doing research on transferable skills between similar occupations. My primary reference is the National Occupation Classification (NOC) developed by the Government of Canada. Based on a given NOC code, I want you to identify 5 similar jobs with a percentage similarity estimate based on overlapping skills and education requirements. The job I need you to analyze is "
 
 
-@app.route("/", methods=["GET", "POST"])
-def get_prompt():
-    if request.method == "POST":
-        recent_job = request.form.get("recentjob")
-        print(recent_job)
-        prompt_text = prompt_example + recent_job
-        print(prompt_text)
-        answer = callChatGpt(prompt_text)
-    return render_template('home.html', answer=answer)
-
-
 def callChatGpt(prompt_text):
     response = openai.Completion.create(
         model="text-davinci-003",
@@ -31,6 +20,17 @@ def callChatGpt(prompt_text):
     )
     answer = response.choices[0].text
     return answer
+
+
+@app.route("/", methods=["GET", "POST"])
+def get_prompt():
+    if request.method == "POST":
+        recent_job = request.form.get("recentjob")
+        print(recent_job)
+        prompt_text = prompt_example + recent_job
+        print(prompt_text)
+        answer = callChatGpt(prompt_text)
+    return render_template('home.html', answer=answer)
 
 
 # callChatGpt("How many bones do a shark have?")
