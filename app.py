81898db66b1df0a67759daccba58e0faf988ec36
Dhaneesh K Kuniyil <dhaneesh.krishnan@gmail.com>
2023-06-15 10:46:10 -0600

    updated app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import openai
 import os
+import json
+from flask import jsonify
 load_dotenv()
 openai.organization = os.getenv("ORG_ID")
 openai.api_key = os.getenv("JOB_ROUTE_KEY")
@@ -9,6 +11,8 @@ answer = ''
 app = Flask(__name__)
 
 prompt_example = "I am doing research on transferable skills between similar occupations. My primary reference is the National Occupation Classification (NOC) developed by the Government of Canada. Based on a given NOC code, I want you to identify 5 similar jobs with a percentage similarity estimate based on overlapping skills and education requirements. The job I need you to analyze is "
+
+prompt_json = "Please provide a response in JSON format only, based on the given NOC code. Include the original job title, and a node called 'similar_jobs' with sub-nodes containing 'job_title', 'noc_code' and 'percent_similarity'"
 
 
 def callChatGpt(prompt_text):
@@ -27,10 +31,11 @@ def get_prompt():
     if request.method == "POST":
         recent_job = request.form.get("recentjob")
         print(recent_job)
-        prompt_text = prompt_example + recent_job
+        prompt_text = prompt_example + recent_job + prompt_json
         print(prompt_text)
-        answer = callChatGpt(prompt_text)
+        answer = json.loads(callChatGpt(prompt_text))
         # return answer
+        print(type(answer))
         return render_template('answer.html', answerToHtml=answer)
 
 
