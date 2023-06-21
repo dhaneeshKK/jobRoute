e386679307cd8dd6ef42407b6287ac2b9e91a0ff
Dhaneesh K Kuniyil <dhaneesh.krishnan@gmail.com>
2023-06-21 12:06:08 -0600

    removed skills

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 import openai
 import os
 import json
+app = Flask(__name__, template_folder="templates")
+cors = CORS(app)
 load_dotenv()
 openai.organization = os.getenv("ORG_ID")
 openai.api_key = os.getenv("JOB_ROUTE_KEY")
 answer = ''
-app = Flask(__name__)
 
 prompt_example = "I am doing research on transferable skills between similar occupations. My primary reference is the National Occupation Classification (NOC) developed by the Government of Canada. Based on a given NOC code, I want you to identify 5 similar jobs with a percentage similarity estimate based on overlapping skills and education requirements. The job I need you to analyze is "
 
@@ -25,26 +27,19 @@ def callChatGpt(prompt_text):
     return answer
 
 
-@app.route("/answer", methods=["GET", "POST"])
-def get_prompt():
-    if request.method == "POST":
-        recent_job = request.form.get("recentjob")
-        print(recent_job)
-        prompt_text = prompt_example + recent_job + prompt_json
-        print(prompt_text)
-        answer = json.loads(callChatGpt(prompt_text))
-        # return answer
-        print(type(answer))
-        return render_template('answer.html', answerToHtml=answer)
-
-
 @app.route("/", methods=["GET", "POST"])
 def job_route():
     return render_template('home.html')
 
 
-# callChatGpt("How many bones do a shark have?")
-# print(callChatGpt("Who is the creator of Tesla car company?"))
+@app.route("/answer", methods=["GET", "POST"])
+def get_prompt():
+    if request.method == "POST":
+        recent_job = request.form.get("recentJob")
+        prompt_text = prompt_example + recent_job + prompt_json
+        answer = json.loads(callChatGpt(prompt_text))
+        print(type(answer))
+        return render_template('answer.html', answerToHtml=answer)
 
 
 if __name__ == "__main__":
